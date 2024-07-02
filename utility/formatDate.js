519b6193d92c41cea6bd3b387a5712ad31559a74
ralux66 <ralux.zepeda@gmail.com>
2024-07-01 19:19:20 -0600

    hora y fecha

--- utility/formatDate.js
+++ utility/formatDate.js
@@ -1,8 +1,14 @@
 
+const moment = require('moment-timezone');
+
 
 const dateFormat = (date) => {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
+    //let partes = date.split('/');
+    //var dec = moment(date).getHours();
+
+    var d = moment.tz(date, 'YYYY-MM-DD', 'America/Mexico_City').format();//new Date(date),
+
+     /*    month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
         year = d.getFullYear();
 
@@ -10,24 +16,29 @@ const dateFormat = (date) => {
         month = '0' + month;
     if (day.length < 2)
         day = '0' + day;
-
-    return [year, month, day].join('-');
+ */
+    //return [year, month, day].join('-');
+    return d;
 };
 
-const obtenerHoraConFormato = (fecha)=> {
+const obtenerHoraConFormato = (date) => {
+    var dec = moment(date);
+
+    var fecha = moment.tz(date, 'YYYY-MM-DD', 'America/Mexico_City').format('HH:mm:ss');
     // Obtener la hora actual
     //var fecha = date;
-    var hora = fecha.getHours();
+  /*   var hora = fecha.getHours();
     var minutos = fecha.getMinutes();
     var segundos = fecha.getSeconds();
 
     // Agregar ceros a la izquierda si es necesario
     if (hora < 10) hora = '0' + hora;
     if (minutos < 10) minutos = '0' + minutos;
-    if (segundos < 10) segundos = '0' + segundos;
+    if (segundos < 10) segundos = '0' + segundos; */
 
     // Crear la cadena de hora con el formato especificado
-    var horaConFormato = hora + ':' + minutos + ':' + segundos;
+    //var horaConFormato = hora + ':' + minutos + ':' + segundos;
+    var horaConFormato = fecha;
 
     return horaConFormato;
 }
